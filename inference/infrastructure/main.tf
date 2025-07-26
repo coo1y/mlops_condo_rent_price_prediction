c12df09271f24720de5035ef38ba133a6f650fff
coo1y <104308184+coo1y@users.noreply.github.com>
2025-07-26 12:58:23 +0700

    add state in an s3 bucket (#5)
    * add state in an s3 bucket
    
    * add state in an s3 bucket
    
    * edit correct backend

--- inference/infrastructure/main.tf
+++ inference/infrastructure/main.tf
@@ -5,6 +5,13 @@ terraform {
     }
   }
 
+  backend "s3" {
+    bucket  = "condo4rent-terraform-state"
+    key     = "condo4rent-prod.tfstate"
+    region  = "ap-southeast-7"
+    encrypt = true
+  }
+
   required_version = "~> 1.12.2"
 }
 
